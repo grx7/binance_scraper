fc9fadd2c256e82821be1b00227ae44cb09ad61f
Clearner <ismike2000@hotmail.com>
2018-02-23 20:36:28 +0800

    update

--- market_data.py
+++ market_data.py
@@ -10,7 +10,6 @@ print (content[0][u'price'])
 
 
 """
-
 #ORDER BOOK
 
 symbol = 'BNTBTC'
@@ -40,10 +39,12 @@ print content
 
 """
 
+""" ** not working **
+"""
 #OLD TRADE LOOKUP(MARKET DATA)
 
 symbol = 'BNTBTC'
-limit = '500'
+#limit = '500'
 endpoint = 'https://api.binance.com'
 path = '/api/v1/historicalTrades?symbol='+symbol #+'&limit='+limit
 url = endpoint+path
@@ -52,3 +53,74 @@ response = requests.get(url)
 content = json.loads(response.text)
 print content
 
+"""
+"""
+
+"""
+#COMPRESSED/AGGREGATE TRADES LIST
+
+symbol = 'BNTBTC'
+#limit = '500'
+endpoint = 'https://api.binance.com'
+path = '/api/v1/aggTrades?symbol='+symbol #+'&limit='+limit
+url = endpoint+path
+
+response = requests.get(url)
+content = json.loads(response.text)
+print content
+
+"""
+
+"""
+#KLINE/CANDLESTICK DATA
+symbol = 'BTCUSDT'
+#limit = '500'
+endpoint = 'https://api.binance.com'
+path = '/api/v1/klines?symbol='+symbol+'&interval=15m' #+'&limit='+limit
+url = endpoint+path
+
+response = requests.get(url)
+content = json.loads(response.text)
+
+
+for num in range(0,499):
+    print (content[num][4])
+
+"""
+
+"""
+# 24HR TICKER PRICE CHANGE STATISTICS
+
+symbol = 'BTCUSDT'
+endpoint = 'https://api.binance.com'
+path = '/api/v1/ticker/24hr?symbol='+symbol
+url = endpoint+path
+
+response = requests.get(url)
+content = json.loads(response.text)
+print content
+"""
+"""
+# SYMBOL PRICE TICKER
+
+symbol = 'NEOUSDT'
+endpoint = 'https://api.binance.com'
+path = '/api/v3/ticker/price?symbol='+symbol
+url = endpoint+path
+
+response = requests.get(url)
+content = json.loads(response.text)
+print content
+"""
+"""
+# SYMBOL ORDER BOOK TICKER
+
+symbol = 'BTCUSDT'
+endpoint = 'https://api.binance.com'
+path = '/api/v3/ticker/bookTicker?symbol='+symbol
+url = endpoint+path
+
+response = requests.get(url)
+content = json.loads(response.text)
+print content
+"""
--- report.md
+++ report.md
@@ -1,1 +1,7 @@
-test this 
+
+
+1.endpoint -> https://api.binance.com
+2.return JSON object or array
+3.for GET endpoint, parameters are sent as a query string.
+4.POST PUT DELETE endpoints, parameters are sent as q query string or request body.
+
